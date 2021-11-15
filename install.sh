c820e51268c5598e36baccc1b6959c13cf7f6600
Panudmet Tammwongsa <3482285+panudetjt@users.noreply.github.com>
2021-11-15 14:15:36 +0700

    add .tmux.conf

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.zprofile ~/.zprofile
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
