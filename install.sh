b92e9d89132aa8adfe685ac5834324373100a0f3
Panudmet Tammwongsa <3482285+panudetjt@users.noreply.github.com>
2022-08-01 12:07:18 +0700

    Fix missing brew

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew bundle install
-
+ln -s $(pwd)/.zprofile ~/.zprofile
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/.zshrc ~/.zshrc
-ln -s $(pwd)/.zprofile ~/.zprofile
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/.vimrc ~/.vimrc
+
+brew bundle install
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
